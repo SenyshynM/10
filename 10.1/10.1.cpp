d7c6dab9f4bd2dc5e033b9a3471a70188dbedcbf
SenyshynM <73256299+SenyshynM@users.noreply.github.com>
2021-04-08 02:05:44 +0300

    1

---
+++ 10.1/10.1.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+bool search(const char* s);
+
+int main()
+{
+	char str[101];
+
+	ifstream f("t.txt");
+	f >> str;
+	if (search(str))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	f.close();
+
+	cin.get();
+	return 0;
+}
+
+bool search(const char* s)
+{
+
+	for (int j = 0; s[j] != '\0'; j++)
+		if ((s[j] == 'a' && s[j + 1] == 'a') || (s[j] == 'b' && s[j + 1] == 'b') || (s[j] == 'c' && s[j + 1] == 'c'))
+			return true;
+	return false;
+}
---
+++ 10.2/10.2/10.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+bool firstletter();
+
+int main()
+{
+    cout << "Word starts with \'a\': ";
+    if (firstletter())
+        cout << "Yes" << endl;
+    else
+        cout << "no" << endl;
+
+    return 0;
+}
+
+bool firstletter()
+{
+    ifstream f;
+    char words[100];
+    f.open("t.txt");
+
+    while (f >> words)
+    {
+        if (words[0] == 'a')
+        {
+            f.close();
+            return true;
+        }
+    }
+
+    f.close();
+    return false;
+}
+
